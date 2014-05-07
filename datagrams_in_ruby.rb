e85e5e3ed071b6ed13c8dd8abbd52bbe9ba804c2
Mohnish G J <mail4mohnish@gmail.com>
2014-05-07 07:01:18 +0530

    Update datagrams_in_ruby.rb

--- datagrams_in_ruby.rb
+++ datagrams_in_ruby.rb
@@ -9,10 +9,10 @@ request_message = ''
 $a = IO.new(2,"w+")
 
 while request_message.chop != "quit"
-    puts "Enter Message: "
-    request_message = $a.readline
+  puts "Enter Message: "
+  request_message = $a.readline
 #puts '{$client_id1} says:' + a
-    ds.send(request_message,0)
+  ds.send(request_message,0)
 #response,address = ds.recvfrom(1024)
 #puts response
 end
