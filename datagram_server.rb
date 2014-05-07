e0cf67cfb1bff16f49bc95be6e0c47f4d712501b
Mohnish G J <mail4mohnish@gmail.com>
2014-05-07 06:59:47 +0530

    Using 2 space indentation

--- datagram_server.rb
+++ datagram_server.rb
@@ -12,44 +12,44 @@ $b = IO.new(2,"w+")
 $client_id = 1
 $clients = []
 $flag = 0
+
 loop do
 
-    request, address = ds.recvfrom(1024)
-    response = request
-    clientaddr = address[3]
-    clientname = address[2]
-    clientport = address[1]
+  request, address = ds.recvfrom(1024)
+  response = request
+  clientaddr = address[3]
+  clientname = address[2]
+  clientport = address[1]
 
-    if ( $clients.empty? )
-        $clients[$client_id-1] = clientport
-        puts "Client #{$client_id} enters the chat room "
-        $client_id += 1
-        puts "#{$clients}"
-    else
-        #puts(r.include? 'a')
-        unless $clients.include? clientport       
-            puts "Client #{$client_id} enters the chat room "
-            $clients[$client_id-1] = clientport
-            $client_id += 1
-        end
-        #    $client_id += 1
+  if ( $clients.empty? )
+    $clients[$client_id-1] = clientport
+    puts "Client #{$client_id} enters the chat room "
+    $client_id += 1
+    puts "#{$clients}"
+  else
+    #puts(r.include? 'a')
+    unless $clients.include? clientport       
+      puts "Client #{$client_id} enters the chat room "
+      $clients[$client_id-1] = clientport
+      $client_id += 1
     end
+        #    $client_id += 1
+  end
 
 #    puts "Connection from :Client #{clientport} says: #{response} #{clientname} #{clientaddr} #{clientport}"
-    if( response.chop != "quit")
-        puts "Client #{clientport}  says: #{response} "
-    else
-        puts "Client #{clientport} leaves the chat room "
-        $client_id -= 1
-    end
+  if( response.chop != "quit")
+    puts "Client #{clientport}  says: #{response} "
+  else
+    puts "Client #{clientport} leaves the chat room "
+    $client_id -= 1
+  end
 
-    if($client_id == 1)
-        print " Do you want to close the chat room(y/n)?: "
-            a = $b.readline
-        if ( a.chop == "y")
-            break
-        end
+  if($client_id == 1)
+    print " Do you want to close the chat room(y/n)?: "
+    a = $b.readline
+    if ( a.chop == "y")
+      break
     end
+  end
 #    ds.send(response, 0, clientaddr, clientport)
-
 end
